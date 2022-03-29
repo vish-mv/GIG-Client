ea533330196385b8e9a79b98de2fc8094d3c681c
umayanga <umayangag@gmail.com>
2022-03-29 16:08:05 +0530

    remove datetime pickers

--- src/components/entity/ValueEditor.js
+++ src/components/entity/ValueEditor.js
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import TextField from "@mui/material/TextField";
 import Divider from '@mui/material/Divider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateTimePicker from '@mui/lab/DateTimePicker';
 
 export default function ValueEditor(props) {
   const {value, setValue} = props;
@@ -15,79 +12,79 @@ export default function ValueEditor(props) {
   }
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <table width="100%">
-        <tbody>
-        <tr>
-          <td>Value Type:</td>
-          <td width="90%">
-            <TextField
-              variant="outlined"
-              size="small"
-              multiline
-              maxRows={5}
-              value={value.value_type}
-              onChange={(e) => setEntityValue(e)}
-              fullWidth/>
-          </td>
-        </tr>
-        <tr>
-          <td>Value String:</td>
-          <td>
-            <TextField
-              variant="outlined"
-              size="small"
-              multiline
-              maxRows={5}
-              value={value.value_string}
-              onChange={(e) => setEntityValue(e)}
-              fullWidth/>
-          </td>
-        </tr>
-        <tr>
-          <td>Source:</td>
-          <td>
-            <TextField
-              variant="outlined"
-              size="small"
-              multiline
-              maxRows={5}
-              value={value.source}
-              onChange={(e) => setEntityValue(e)}
-              fullWidth/>
-          </td>
-        </tr>
-        <tr>
-          <td>Date:</td>
-          <td>
-            <TextField
-              variant="outlined"
-              size="small"
-              multiline
-              maxRows={5}
-              value={value.date}
-              onChange={(e) => setEntityValue(e)}
-              fullWidth/>
-          </td>
-        </tr>
-        <tr>
-          <td>Updated at:</td>
-          <td>
-            <DateTimePicker
-              value={new Date(value)}
-              onChange={(e) => setEntityValue(e)}
-              fullWidth
-              renderInput={(params) => <TextField size="small" {...params} />}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <Divider/>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </LocalizationProvider>
+    <table width="100%">
+      <tbody>
+      <tr>
+        <td>Value Type:</td>
+        <td width="90%">
+          <TextField
+            variant="outlined"
+            size="small"
+            multiline
+            maxRows={5}
+            value={value.value_type}
+            onChange={(e) => setEntityValue(e)}
+            fullWidth/>
+        </td>
+      </tr>
+      <tr>
+        <td>Value String:</td>
+        <td>
+          <TextField
+            variant="outlined"
+            size="small"
+            multiline
+            maxRows={5}
+            value={value.value_string}
+            onChange={(e) => setEntityValue(e)}
+            fullWidth/>
+        </td>
+      </tr>
+      <tr>
+        <td>Source:</td>
+        <td>
+          <TextField
+            variant="outlined"
+            size="small"
+            multiline
+            maxRows={5}
+            value={value.source}
+            onChange={(e) => setEntityValue(e)}
+            fullWidth/>
+        </td>
+      </tr>
+      <tr>
+        <td>Date:</td>
+        <td>
+          <TextField
+            variant="outlined"
+            size="small"
+            multiline
+            maxRows={5}
+            value={value.date}
+            onChange={(e) => setEntityValue(e)}
+            fullWidth/>
+        </td>
+      </tr>
+      <tr>
+        <td>Updated at:</td>
+        <td>
+          <TextField
+            variant="outlined"
+            size="small"
+            multiline
+            maxRows={5}
+            value={value.date}
+            onChange={(e) => setEntityValue(e)}
+            fullWidth/>
+        </td>
+      </tr>
+      <tr>
+        <td colSpan={2}>
+          <Divider/>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   )
 }
