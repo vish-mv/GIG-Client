3edabdc008178d23e297b82420cc45658a74b0a0
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-09-13 16:23:02 +0530

    Update  configs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const API_URL = window?.configs?.serviceURL ? window.configs.serviceURL : "/";
+
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/images', {target: process.env.REACT_APP_SERVER_URL, changeOrigin: true}));
+  app.use(createProxyMiddleware('/images', {target: API_URL, changeOrigin: true}));
 };
