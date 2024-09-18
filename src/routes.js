bac4730906764ac626b9406ee8035c87fb1a6f72
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-09-18 09:02:44 +0530

    change routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
+const API_URL = window?.configs?.serviceURL ? window.configs.serviceURL : "/";
+
 export const AppRoutes = {
-  home: '/',
-  search: '/search/',
-  entity: '/content/',
-  edit: '/edit/',
-  login: '/login',
-  register: '/register',
-  graph: '/graphs/visualize'
+  home: API_URL+'/',
+  search: API_URL+'/search/',
+  entity: API_URL+'/content/',
+  edit: API_URL+'/edit/',
+  login: API_URL+'/login',
+  register: API_URL+'/register',
+  graph: API_URL+'/graphs/visualize'
 };
